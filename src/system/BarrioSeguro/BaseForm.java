25c2b5643e3e3b8f89c010519d52d528ccbe3a87
7equinox <116896327+7equinox@users.noreply.github.com>
2025-02-01 01:12:50 +0800

    Update BaseForm.java

--- src/system/BarrioSeguro/BaseForm.java
+++ src/system/BarrioSeguro/BaseForm.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
 import java.io.File;
 
 import java.sql.Connection;
